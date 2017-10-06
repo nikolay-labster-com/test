8aa26d9d03b500c0f429acfda8f021248a27d896
Nikolay Uvaliyev <nikolay@labster.com>
2017-10-06 08:41:41 +0600

    123

--- proj/ConsoleApplication1/Class1.cs
+++ proj/ConsoleApplication1/Class1.cs
@@ -8,5 +8,7 @@ namespace ConsoleApplication1
 {
 	class Class1
 	{
+
+		// asdf asdf
 	}
 }
--- proj/ConsoleApplication1/Program.cs
+++ proj/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-			// something
+			// something 2
             //change 1
             // change 2
 
