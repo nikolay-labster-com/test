52325e19c34cd379bd8f4c1a2b2b362da1ca3e4f
Nikolay Uvaliyev <nikolay@labster.com>
2017-10-07 14:23:21 +0600

    commit CRLF from VS

--- proj/ConsoleApplication1/Class2.cs
+++ proj/ConsoleApplication1/Class2.cs
@@ -9,5 +9,6 @@ namespace ConsoleApplication1
 	class Class2
 	{
 		// changes in file which is stored locally as CRLF, and on server LF. VS shows file as changed. commit from sourcetree
+		// one more change in the same file (CRLF locally, LF remotely) to commit from VS
 	}
 }
